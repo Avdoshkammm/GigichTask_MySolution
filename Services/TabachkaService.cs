51e1a49116fec54cec7bd4729ba98ef3300fc566
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-05-06 13:40:43 +0300

    Создали конструктор на сервис

---
+++ Services/TabachkaService.cs
@@ -0,0 +1,16 @@
+﻿using GigichTask_MySolution_.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GigichTask_MySolution_.Services
+{
+    public class TabachkaService : ControllerBase
+    {
+        private readonly TabachkaContext _context;
+
+        public TabachkaService(TabachkaContext context)
+        {
+            _context = context;
+        }
+    }
+}
