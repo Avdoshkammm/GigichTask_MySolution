c04c01f58e29a964ddfd63a4f44fdb4983efc478
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-05-06 13:18:39 +0300

    Подключил строку подключения в файл program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using GigichTask_MySolution_.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TabachkaContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("TabachkaCS")));
+
 
 var app = builder.Build();
 
