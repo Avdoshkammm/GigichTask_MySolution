3d8ff9d05b26479d71d8e59234aa1dfb3d1419dc
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-05-06 13:10:02 +0300

    Создание класса для БД

---
+++ Models/Tabachka.cs
@@ -0,0 +1,10 @@
+﻿namespace GigichTask_MySolution_.Models
+{
+    public class Tabachka
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Cost { get; set; }
+        public int Remains { get; set; }
+    }
+}
