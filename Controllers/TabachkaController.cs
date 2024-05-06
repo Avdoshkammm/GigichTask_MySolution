b658b6c2904378fef6651dd8cdee3c269973fda5
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-05-06 13:26:46 +0300

    Создан конструктор контроллера

---
+++ Controllers/TabachkaController.cs
@@ -0,0 +1,20 @@
+﻿using GigichTask_MySolution_.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GigichTask_MySolution_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TabachkaController : ControllerBase
+    {
+        private readonly TabachkaContext _dbContext;
+
+        public TabachkaController(TabachkaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
