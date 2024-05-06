f975b13ccecdfa41614791084035a6fbaf031da7
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-05-06 13:15:06 +0300

    Создал класс context

---
+++ Models/TabachkaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GigichTask_MySolution_.Models
+{
+    public class TabachkaContext : DbContext
+    {
+        public TabachkaContext(DbContextOptions<TabachkaContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tabachka> Tabak { get; set; }
+    }
+}
